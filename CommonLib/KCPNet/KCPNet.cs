0462c637e31d0aeab917303ae52fde53aca524a8
752636090 <752636090@qq.com>
2021-06-22 16:08:34 +0800

    KCPNet 108颜色日志工具

---
+++ CommonLib/KCPNet/KCPNet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace KCPNet
+{
+    public class KCPNet
+    {
+        private UdpClient udp;
+        private IPEndPoint remotePoint;
+
+        #region 客户端
+        public void StartAsClient(string ip, int port)
+        {
+            udp = new UdpClient(0);
+            remotePoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            KCPTool.ColorLog(ConsoleColor.Green, "Client Start...");
+        }
+        #endregion
+    }
+}
---
+++ CommonLib/KCPNet/KCPTool.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace KCPNet
+{
+    public class KCPTool
+    {
+        public static Action<string> LogFunc;
+        public static Action<ConsoleColor, string> ColorLogFunc;
+        public static Action<string> WarningFunc;
+        public static Action<string> ErrorFunc;
+
+        public static void Log(string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (LogFunc != null)
+            {
+                LogFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg);
+            }
+        }
+
+        public static void ColorLog(ConsoleColor color, string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (ColorLogFunc != null)
+            {
+                ColorLogFunc(color, msg);
+            }
+            else
+            {
+                ConsoleLog(msg, color);
+            }
+        }
+
+        public static void Warning(string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (WarningFunc != null)
+            {
+                WarningFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg, ConsoleColor.DarkYellow);
+            }
+        }
+
+        public static void Error(string msg, params object[] args)
+        {
+            msg = string.Format(msg, args);
+            if (ErrorFunc != null)
+            {
+                ErrorFunc(msg);
+            }
+            else
+            {
+                ConsoleLog(msg, ConsoleColor.DarkRed);
+            }
+        }
+
+        private static void ConsoleLog(string msg, ConsoleColor color = ConsoleColor.Gray)
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            msg = $"Thread:{threadId} {msg}";
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
