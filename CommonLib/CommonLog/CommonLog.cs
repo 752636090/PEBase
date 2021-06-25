ba31c97d9c557cd14355960e83a8260a882e3f8f
752636090 <752636090@qq.com>
2021-06-25 17:18:36 +0800

    CommonLog 005组装标记信息与堆栈信息

--- CommonLib/CommonLog/CommonLog.cs
+++ CommonLib/CommonLog/CommonLog.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
 
 namespace Utils
 {
-    public class CommonLog
+    public static class CommonLog
     {
         public static LogConfig LogConfig { get; private set; }
         private static ILogger logger;
@@ -32,8 +35,47 @@ namespace Utils
             {
                 return;
             }
-            msg = string.Format(msg, args);
+            msg = DecorateLog(string.Format(msg, args));
             logger.Log(msg);
+        }
+
+        private static string DecorateLog(string msg, bool isTrace = false)
+        {
+            StringBuilder sb = new StringBuilder(LogConfig.LogPrefix, 100);
+            if (LogConfig.EnableTime)
+            {
+                //sb.Append($" {DateTime.Now.ToString("hh:mm:ss--fff")}");
+                sb.Append($" {DateTime.Now:hh:mm:ss--fff}");
+            }
+            if (LogConfig.EnableThreadId)
+            {
+                sb.Append($" {GetThreadId()}");
+            }
+            sb.Append($" {LogConfig.LogSeperate} {msg}");
+            if (isTrace)
+            {
+                sb.Append($" \nStackTrace:{GetLogTrace()}");
+            }
+            return sb.ToString();
+        }
+
+        private static string GetThreadId()
+        {
+            return $" ThreadId:{Thread.CurrentThread.ManagedThreadId}";
+        }
+
+        private static string GetLogTrace()
+        {
+            StackTrace trace = new StackTrace(3, true);
+
+            string traceInfo = "";
+            for (int i = 0; i < trace.FrameCount; i++)
+            {
+                StackFrame frame = trace.GetFrame(i);
+                traceInfo += $"\n\t{frame.GetFileName()}::{frame.GetMethod()} line:{frame.GetFileLineNumber()}";
+            }
+
+            return traceInfo;
         }
     }
 }
