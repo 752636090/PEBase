ca32a97b835e048ee16416dc9ac360ce8e671996
752636090 <752636090@qq.com>
2021-06-28 16:03:27 +0800

    FixedPhysx 103逻辑帧使用FixedUpdate驱动

--- Assets/FixedPhysx/Examples/TestFixedPhysx.cs
+++ Assets/FixedPhysx/Examples/TestFixedPhysx.cs
@@ -11,9 +11,16 @@ public class TestFixedPhysx : MonoBehaviour
 
         CommonLog.InitSettings(new LogConfig { LogType = Utils.LogType.Unity });
         this.Log("初始化日志工具完毕.");
+
+        Time.fixedDeltaTime = 0.667f;
     }
 
     private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
     {
         
     }
